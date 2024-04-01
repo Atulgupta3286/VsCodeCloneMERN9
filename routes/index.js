501cec8251da0f1ad0e9339b72bf872a894fa858
Atul Gupta <princegupta9302@gmail.com>
2024-04-02 01:13:51 +0530

    5-commit-Day5-19-march

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,4 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 const fs = require("fs");
 const path = require("path");
@@ -9,26 +9,28 @@ function readdir() {
 }
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', {
+router.get("/", function (req, res, next) {
+  res.render("index", {
     files: readdir(),
-    filedata: ""
+    filedata: "",
+    filename: "",
   });
 });
 
-router.get('/file/:filename', function (req, res, next) {
+router.get("/file/:filename", function (req, res, next) {
   const filedata = fs.readFileSync(
     path.join(globalpath, req.params.filename),
     "utf-8"
   );
-  console.log('f', filedata)
-  res.render('index', {
+  console.log("f", filedata);
+  res.render("index", {
     files: readdir(),
-    filedata: filedata
+    filedata: filedata,
+    filename: req.params.filename,
   });
 });
 
-router.post('/createfile', function (req, res, next) {
+router.post("/createfile", function (req, res, next) {
   const filename = req.body.filename;
   // const {filename}  = req.body;
 
@@ -36,11 +38,17 @@ router.post('/createfile', function (req, res, next) {
   res.redirect(`/file/${filename}`);
 });
 
-router.get('/delete/:filename', function (req, res, next) {
-  fs.unlinkSync(
-    path.join(globalpath, req.params.filename));
-  res.redirect("/")
+router.get("/delete/:filename", function (req, res, next) {
+  fs.unlinkSync(path.join(globalpath, req.params.filename));
+  res.redirect("/");
+});
+
+router.post("/update/:filename", function (req, res, next) {
+  fs.writeFileSync(
+    path.join(globalpath, req.params.filename),
+    req.body.filedata
+  );
+  res.redirect(`/file/${req.params.filename}`);
 });
 
 module.exports = router;
-
